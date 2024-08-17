323cd66cf1684afb7f6373006b3708451dbf9086
Samuel Javorka <samkoj@hotmail.co.uk>
2024-08-17 23:22:42 +0100

    useRef: track a variable 'timer' between rerenders

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,42 @@
+import { useState, useRef } from "react";
+
+// storing something outside of the component means all instances use this.
+// let timer;
+// storing a plain varable in the component means it will be reset on rerender to how it's defined initially
+// therefore, useRef can be used inside of the component as the value is not lost on rerender, similar to state. However, unlike state, setting the value does not cause a rerender / reexecution.
+
 export default function TimerChallenge({ title, targetTime }) {
+	const timer = useRef();
+
+	const [timerStarted, setTimerStarted] = useState(false);
+	const [timerExpired, setTimerExpired] = useState(false);
+
+	function handleStart() {
+		timer.current = setTimeout(() => {
+			setTimerExpired(true);
+		}, targetTime * 1000);
+
+		setTimerStarted(true);
+	}
+
+	function handleStop() {
+		clearTimeout(timer.current);
+		setTimerStarted(false);
+	}
+
 	return (
 		<section className="challenge">
 			<h2>{title}</h2>
+			{timerExpired && <p>You lost!</p>}
 			<p className="challenge-time">
 				{targetTime} second{targetTime > 1 ? "s" : ""}
 			</p>
 			<p>
-				<button>Start Challenge</button>
+				<button onClick={timerStarted ? handleStop : handleStart}>
+					{timerStarted ? "Stop" : "Start Challenge"}
+				</button>
 			</p>
-			<p className="">Time is running... / Timer inactive</p>
+			<p className={timerStarted ? "active" : undefined}>Time is running... / Timer inactive</p>
 		</section>
 	);
 }
